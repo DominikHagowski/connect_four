db1fca01c8bd46c8641accf4ffc0a30733c4646d
= <dom@inik.dev>
2021-07-25 11:41:44 +0000

    turns and playerlists

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 
 //set all of the colours used by the program
-static COLOR_P1: u8 = 196; //red
-static COLOR_P2: u8 = 27; //blue
-static COLOR_SEL: u8 = 180; //selected colour
+const COLOR_P1: u8 = 196; //red
+const COLOR_P2: u8 = 27; //blue
+const COLOR_SEL: u8 = 180; //selected colour
 
 //set all of the dimensions of the board
 static BOARD_WIDTH: u8 = 7;
@@ -32,11 +32,31 @@ fn color_reset() {
     print!("\x1b[0m");
 }
 
+enum PlayerType {
+    Local,
+    AI,
+    Net,
+}
+
+struct Player {
+    p_type: PlayerType,
+    color: u8,
+}
+
+enum GameState {
+    In_Progress,
+    Finished,
+}
+
+struct Result {
+    State: GameState,
+}
+
 fn draw_board() {
     cursor_home();
     
-    for y in 1..BOARD_HEIGHT {
-        for x in 1..BOARD_WIDTH {
+    for _y in 1..BOARD_HEIGHT {
+        for _x in 1..BOARD_WIDTH {
             print!("O");
         }
         print!("\n");
@@ -45,10 +65,46 @@ fn draw_board() {
     cursor_home();
 }
 
+fn execute_turn(player: &Player) -> Result {
+    color_set_foreground(player.color);
+
+    print!("TESTESTESTESTESTEST");
+
+    let Out: Result = Result {
+        State: GameState::Finished,
+    };
+
+    return Out;
+}
+
 fn main() {
     clear_screen();
 
     draw_board();
+
+    const p1: Player = Player {
+        p_type: PlayerType::Local,
+        color: COLOR_P1,
+    };
+
+    const p2: Player = Player {
+        p_type: PlayerType::Local,
+        color: COLOR_P2,
+    };
+
+    let players: [Player; 2] = [p1, p2];
+    let mut turn = 1;
+
+    loop {
+        let state: Result = execute_turn(&(players[turn]));
+        turn = (turn + 1) % 2;
+
+        match state.State {
+            GameState::Finished => {break;},
+            GameState::In_Progress => {continue;},
+        }
+
+    }
 
     cursor_goto(0,20);
 }
