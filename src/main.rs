e0abc3b82b418c11470c31cfba0338a14f06b05b
mzyzc <mzyzc@czyz.xyz>
2021-08-02 20:20:25 +0100

    Style changes to reduce noise; removed parentheses around conditionals, capitalised constants, made newlines in print macros clearer

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,9 @@ fn main() {
 
     println!("The game is now setting up");
     
-    const width: usize = 7;
-    const height: usize = 6;
-    let mut board = [[Cell::Unset; width]; height];
+    const WIDTH: usize = 7;
+    const HEIGHT: usize = 6;
+    let mut board = [[Cell::Unset; WIDTH]; HEIGHT];
 
     let mut turn: u8 = 0;
 
@@ -59,11 +59,11 @@ fn main() {
         // Print the numbers above the board
         // for easier user input
         let numbers = "1234567890";
-        print!("{}", &numbers[..(width - 1)]);
-        print!("\n");
+        print!("{}", &numbers[..(WIDTH - 1)]);
+        println!();
 
-        for y in 0..width {
-            for x in 0..height {
+        for y in 0..WIDTH {
+            for x in 0..HEIGHT {
 
                 match &board[x][y] {
                     Cell::Unset => {color_reset();},
@@ -84,7 +84,8 @@ fn main() {
         loop {
             let current_player = turn % 2;
 
-            println!("Player {}, input x coord, \nnumbers above the width will be wrapped around", current_player + 1);
+            println!("Player {}, input x coord,", current_player + 1);
+            println!("numbers above the WIDTH will be wrapped around");
 
             let mut input = String::new();
 
@@ -98,21 +99,21 @@ fn main() {
 
             print!("{}", input_num);
 
-            let mut board_min = height;
+            let mut board_min = HEIGHT;
 
             loop {
 
-                if(Cell::Unset == board[input_num as usize][board_min]){
+                if Cell::Unset == board[input_num as usize][board_min] {
                     break;
                 }
 
-                if (board_min == 0) {
+                if board_min == 0 {
                     break;
                 }
                 board_min = board_min - 1;
             }
 
-            if (current_player == 0){
+            if current_player == 0{
                 board[input_num as usize][board_min] = Cell::P2;
             } else {
                 board[input_num as usize][board_min] = Cell::P1;
@@ -129,25 +130,25 @@ fn main() {
 
         Winner = Cell::Unset;
 
-        for y in 0..(height - 1) {
-            for x in 0..(width - 1) {
+        for y in 0..(HEIGHT - 1) {
+            for x in 0..(WIDTH - 1) {
                 let current_player = &board[x][y];
 
-                if (&Cell::Unset == current_player) {
+                if &Cell::Unset == current_player {
                     continue;
                 }
 
-                if (x + 3 < width && 
+                if x + 3 < WIDTH && 
                     &board[x + 1][y] == current_player &&
                     &board[x + 2][y] == current_player &&
-                    &board[x + 3][y] == current_player) {
+                    &board[x + 3][y] == current_player {
                     Winner = Cell::P1;
                 }
 
-                if (y + 3 < height && 
+                if y + 3 < HEIGHT && 
                     &board[x][y + 1] == current_player &&
                     &board[x][y + 2] == current_player &&
-                    &board[x][y + 3] == current_player) {
+                    &board[x][y + 3] == current_player {
                     Winner = Cell::P2;
                 }
             }
@@ -158,12 +159,9 @@ fn main() {
             Cell::P1 => {println!("\nWinner: player 1"); break;},
             Cell::P2 => {println!("\nWinner: player 2"); break;},
         }
-
-        break;
-
     }
 
     // EXIT
-    print!("\n");
+    println!();
     println!("The game is now over");
 }
