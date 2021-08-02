1daded2d6400f023493320ee35158bae5fac9d24
= <dom@inik.dev>
2021-08-02 13:37:28 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 
 //set all of the colours used by the program
 const COLOR_P1: u8 = 196; //red
@@ -41,15 +42,26 @@ fn main() {
     cursor_home();
 
     println!("The game is now setting up");
+    
     const width: usize = 7;
     const height: usize = 6;
     let mut board = [[Cell::Unset; width]; height];
+
+    let mut turn: u8 = 0;
 
     println!("The game is now starting");
 
     loop {
         // DRAW
-   
+        clear_screen();
+        cursor_goto(0, 1);
+
+        // Print the numbers above the board
+        // for easier user input
+        let numbers = "1234567890";
+        print!("{}", &numbers[..(width - 1)]);
+        print!("\n");
+
         for y in 0..width {
             for x in 0..height {
 
@@ -64,11 +76,38 @@ fn main() {
             print!("\n")
         }
 
+        print!("\n");
+
         // INPUT
+
+        let current_player = turn % 2;
+
+        println!("Player {}, input x coord between 1 - 9", current_player + 1);
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input.");
+
+        let inputtest = &input[..1];
+
+        let input_num: u8 = (inputtest.parse::<u8>().unwrap()) - 1;
+
+        print!("{}", input_num);
+
+        if (current_player == 0){
+            board[input_num as usize][4] = Cell::P2;
+        } else {
+            board[input_num as usize][4] = Cell::P1;
+        }
+        
+
+        turn = (turn + 1) % 255;
 
         // CHECK
     
-        break;
+        //break;
     }
 
     // EXIT
